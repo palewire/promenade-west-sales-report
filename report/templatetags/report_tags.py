9f59d19a0ea45996f387bbe032aef3d3fd220f24
palewire <ben.welsh@gmail.com>
2019-03-24 11:11:11 -0700

    Started working in more

--- report/templatetags/report_tags.py
+++ report/templatetags/report_tags.py
@@ -1,5 +1,6 @@
-from django.utils import timezone
+import statistics
 from django import template
+from django.utils import timezone
 register = template.Library()
 
 
@@ -11,3 +12,9 @@ def now():
 @register.simple_tag
 def percent_change(old, new):
     return ((new - old) / old)*100
+
+
+@register.simple_tag
+def average(object_list, attr="count"):
+    values = [o[attr] for o in object_list]
+    return statistics.mean(values)
--- report/views.py
+++ report/views.py
@@ -1,6 +1,10 @@
 from sales.models import Sale
+from django.conf import settings
+from django.utils import timezone
+from django.db.models import Count
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.db.models.functions import TruncYear
 
 
 class IndexView(TemplateView):
@@ -9,15 +13,25 @@ class IndexView(TemplateView):
     def get_context_data(self):
         everything = Sale.objects.select_related("building").all()
         everything_pwest = everything.filter(building__name='Promenade West')
+
         the_last_year = Sale.objects.select_related("building").the_last_year()
         the_last_year_pwest = the_last_year.filter(building__name='Promenade West')
+
         the_year_before = Sale.objects.select_related("building").the_year_before()
         the_year_before_pwest = the_year_before.filter(building__name='Promenade West')
+
+        everything_pwest_by_year = everything_pwest.annotate(
+            year=TruncYear("date")
+        ).filter(
+            date__year__lt=timezone.now().year
+        ).values('year').annotate(count=Count("*")).order_by("year")
+
         return {
             'the_last_year': the_last_year,
             'the_last_year_pwest': the_last_year_pwest,
             'the_year_before': the_year_before,
             'the_year_before_pwest': the_year_before_pwest,
             "everything": everything,
-            "everything_pwest": everything_pwest
+            "everything_pwest": everything_pwest,
+            "everything_pwest_by_year": everything_pwest_by_year,
         }
